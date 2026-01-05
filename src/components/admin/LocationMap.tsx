63df0b057a780f382da6195738bf9cefb815eb0f
sodaaw <ghk132435@naver.com>
2026-01-05 16:24:47 +0900

    배포 에러

--- src/components/admin/LocationMap.tsx
+++ src/components/admin/LocationMap.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './LocationMap.css'
